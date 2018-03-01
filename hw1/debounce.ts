a98686fd9ea632e4ef78ac6c5c4f37ff099e56f1
sholast <kozelalexey@gmail.com>
2018-03-01 23:22:59 +0200

    debounce decorator

--- hw1/debounce.ts
+++ hw1/debounce.ts
@@ -1,4 +1,4 @@
-function debounce(func: Function, delay: number) {
+function debounceWrap(func: Function, delay: number) {
     let timeout: number;
     return function(this: any) {
         let self = this;
@@ -9,5 +9,20 @@ function debounce(func: Function, delay: number) {
             console.log(args);
             func.apply(self, args);
         }, delay);
+    }
+}
+
+function debounce(delay: number) {
+    return function (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>): TypedPropertyDescriptor<any> {
+        const originalMethod = descriptor.value;
+        descriptor.value = debounceWrap(originalMethod, delay);
+        return descriptor;
+    }
+}
+
+class someCLass {
+    @debounce(1000)
+    meth(arg: string) {
+        console.log(arg);
     }
 }
